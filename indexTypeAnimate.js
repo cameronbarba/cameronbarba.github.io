6b4ed6170c3553514e1504882cb0b3479a72c14b
Cameron Barba <barbaca@oregonstate.edu>
2018-11-04 16:13:14 -0800

    rough draft type animation using JavaScript

---
+++ indexTypeAnimate.js
@@ -0,0 +1,15 @@
+var i = 0;
+var text = "My name is Cameron Barba. <br> <br> I'm a Chicano developer, designer, and writer based out of California. <br> I'm a member of HOUND alongside some dope humans.'";
+var speed = 50;
+
+function typeText (){
+	if (i < text.length)
+	{
+		document.getElementById("typeText").innerHTML += text.charAt(i);
+		i++;
+		setTimeout(typeText, speed);
+	}
+}
+
+// pseudeocode
+// I need to make an array or probab
